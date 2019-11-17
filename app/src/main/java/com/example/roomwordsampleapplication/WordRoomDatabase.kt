dc6c4dcf0fe67ef8bfefbca6967343324c7328bd
Tom <melooh42@gmail.com>
2019-11-18 01:34:26 +0900

    Add Adapter and modify model and vm

--- app/src/main/java/com/example/roomwordsampleapplication/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomwordsampleapplication/WordRoomDatabase.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.roomwordsampleapplication.data.Word
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 public abstract class WordRoomDatabase : RoomDatabase() {
@@ -16,7 +19,7 @@ public abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
@@ -26,10 +29,35 @@ public abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 return instance
             }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
         }
     }
 
